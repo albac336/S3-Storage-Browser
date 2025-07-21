6e128a2fdd240a5511c99a64fb4ea9c6d5982772
Matteo <matteopescarolo@ipsoft.srl>
2025-07-21 13:41:35 +0200

    Update resource.ts

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,14 +3,14 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'myStorageBucket',
   isDefault: true,
-   access: (allow) => ({
+  access: (allow) => ({
     'public/*': [
-        allow.guest.to(['read', 'write']),
-        allow.authenticated.to(['read', 'write', 'delete']),
+      allow.authenticated.to(['read', 'write', 'delete']),
+      allow.groups(['admin']).to(['read', 'write', 'delete']),
     ],
     'admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['list'])
+      allow.groups(['admin']).to(['read', 'write', 'delete']),
+      allow.authenticated.to(['list']),
     ],
-   })
+  }),
 });
