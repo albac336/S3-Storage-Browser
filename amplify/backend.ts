cc176d25aed2646cdbf943e0cfc096ef815d5e3a
albac336 <matteopescarolo@ipsoft.srl>
2025-07-21 10:25:10 +0200

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,4 +1,5 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
 import { storage } from './storage/resource';
 
 
@@ -6,5 +7,6 @@ import { storage } from './storage/resource';
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
+  auth,
   storage 
 });
